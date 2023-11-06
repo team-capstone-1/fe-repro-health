1f9c7ceec4f56896f105ddbd2f379819ada90885
alfhiyana <107467634+amorazl@users.noreply.github.com>
2023-11-06 16:16:26 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1 className="bg-green-900">ASNDAKSMAKS</h1>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      "green-900": "#0a5344",
+      "green-800": "#0d6d5a",
+      "green-700": "#108d74",
+      "green-600": "#15b494",
+      "green-500": "#17c6a3",
+      "green-400": "#45d1b5",
+      "green-300": "#64d9c1",
+      "green-200": "#94e5d5",
+      "green-100": "#b7ede2",
+      "green-50": "#e8f9f6",
+    },
+  },
+  plugins: [],
+};
