8a98532587ead6ff89ae880ac33575256a2cb341
aaaroz <ram.ardiansyah18@gmail.com>
2023-11-07 22:04:53 +0700

    feat : set container padding on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "9.75rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+
     extend: {
       colors: {
         green: {
