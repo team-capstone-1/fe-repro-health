79f58261963553131ef505f454552153330890e2
alfhiyana <107467634+amorazl@users.noreply.github.com>
2023-11-07 10:21:07 +0700

    install libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
-import { useState } from "react";
-import "./App.css";
+import logo from "../public/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <h1 className="bg-green-900">ASNDAKSMAKS</h1>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <main className="font-body">
+        <h1 className="text-positive">Vite + React</h1>
+        <img src={logo} alt="" />
+      </main>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      body: ["Poppins", "sans-serif"],
+    },
     colors: {
       "green-900": "#0a5344",
       "green-800": "#0d6d5a",
@@ -14,6 +17,27 @@ export default {
       "green-200": "#94e5d5",
       "green-100": "#b7ede2",
       "green-50": "#e8f9f6",
+
+      "grey-10": "#FBFBF",
+      "grey-50": "#e9e9e9",
+      "grey-100": "#b9b9b9",
+      "grey-200": "#989898",
+      "grey-300": "#686868",
+      "grey-400": "#4b4b4b",
+      "grey-500": "#1e1e1e",
+      "grey-600": "#1b1b1b",
+      "grey-700": "#151515",
+      "grey-800": "#111111",
+      "grey-900": "#0d0d0d",
+
+      negative: "#FC4547",
+      "negative-25": "#FED0D1",
+      warning: "#FEA53F",
+      "warning-25": "#FEE8CF",
+      link: "#3EAFFF",
+      "link-25": "#CEEBFF",
+      positive: "#37C976",
+      "positive-25": "#37C976",
     },
   },
   plugins: [],
