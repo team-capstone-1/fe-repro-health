45b64c09f5546e27d94ae24b69374ed1226dd0c1
aaaroz <ram.ardiansyah18@gmail.com>
2023-11-07 22:09:52 +0700

    refactor(home) : change some styling

--- src/views/app-views/landing-page/index.jsx
+++ src/views/app-views/landing-page/index.jsx
@@ -6,9 +6,9 @@ import ButtonAppStore from "@/components/shared-components/ButtonAppStore";
 export default function LandingPage() {
   return (
     <>
-      <header className="relative pt-8 bg-green-50">
-        <div className="grid grid-cols-1 md:grid-cols-2 h-[34.6rem] bg-center bg-vector-header">
-          <div className="mx-10 md:w-[40rem] md:mx-28 pt-20 z-10 relative">
+      <header className="relative pt-8 h-[65rem] md:h-[68rem] lg:h-[45rem] bg-green-50">
+        <div className="container static grid grid-cols-1 lg:grid-cols-2 h-[34.6rem] bg-center bg-vector-header">
+          <div className="pt-5 md:w-[40rem] md:pt-20 z-10">
             <h3 className="text-green-500">ReproHealth</h3>
             <h1 className="text-green-900">
               Partner Terpercaya untuk Kesehatan Reproduksi Anda
@@ -20,29 +20,26 @@ export default function LandingPage() {
                 kesehatan reproduksi.
               </p>
             </div>
-            <div className="flex mt-10 gap-2 md:gap-10">
+            <div className="flex mt-16 gap-2 md:gap-10">
               <ButtonAppStore />
               <ButtonGooglePlay />
             </div>
           </div>
-          <div className="relative">
+          <div className="static">
+            <div className="absolute right-0 -bottom-0 w-[40rem] h-[20rem] md:w-[44rem] md:h-[30rem] bg-ellipse-header"></div>
             <div
-              className="absolute right-0 -bottom-[30rem] w-[40rem] h-[20rem] sm:w-[44rem] sm:h-[30rem] lg:w-[44rem] lg:h-[30rem] bg-ellipse-header 
-            md:absolute md:right-0 md:-bottom-[30rem] lg:absolute lg:right-0 lg:-bottom-28"
-            ></div>
-            <div
-              className="hidden sm:block sm:absolute sm:right-0 sm:-bottom-[30rem] sm:w-[36.25rem] sm:h-[39.56rem] lg:w-[36.25rem] lg:h-[39.56rem] 
-            md:absolute md:right-24 md:-bottom-[30rem] lg:absolute lg:right-14 lg:-bottom-28 bg-doctor-header"
+              className="hidden sm:block sm:absolute sm:right-0 sm:-bottom-0 w-[36rem] h-[38rem] 
+            md:right-24 md:-bottom-0 lg:absolute lg:right-14 bg-doctor-header"
             ></div>
             <img
               src={doctorImg}
               alt="doctor"
-              className="absolute items-center -bottom-[30rem] sm:hidden"
+              className="absolute right-2 -bottom-0 sm:hidden"
             />
           </div>
         </div>
       </header>
-      <section className="flex my-32  p-2 bg-black">
+      <section className="flex p-2 bg-green-100">
         <div className="w-10 h-10 bg-green-50"></div>
         <div className="w-10 h-10 bg-green-100"></div>
         <div className="w-10 h-10 bg-green-200"></div>
@@ -54,7 +51,7 @@ export default function LandingPage() {
         <div className="w-10 h-10 bg-green-800"></div>
         <div className="w-10 h-10 bg-green-900"></div>
       </section>
-      <section className="flex my-32  p-2 bg-black">
+      {/* <section className="flex my-32 p-2 bg-black">
         <div className="w-10 h-10 bg-grey-10"></div>
         <div className="w-10 h-10 bg-grey-50"></div>
         <div className="w-10 h-10 bg-grey-100"></div>
@@ -66,7 +63,7 @@ export default function LandingPage() {
         <div className="w-10 h-10 bg-grey-700"></div>
         <div className="w-10 h-10 bg-grey-800"></div>
         <div className="w-10 h-10 bg-grey-900"></div>
-      </section>
+      </section> */}
     </>
   );
 }
