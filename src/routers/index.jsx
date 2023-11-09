e81bc6e9c4f56d3d3f558f52629f9834cf0daad5
Aurelia Amanda Robert <aurelia.amanda@ui.ac.id>
2023-11-09 17:20:05 +0800

    feat: Add User Terms page

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Faq from "@/views/app-views/landing-page/misc/Faq";
 import LandingPage from "@/views/app-views/landing-page";
+import UserTerms from "../views/app-views/landing-page/misc/UserTerms";
 
 export default function SetupRoutes() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/faq" element={<Faq />} />
+      <Route path="/userterms" element={<UserTerms />} />
     </Routes>
   );
 }
