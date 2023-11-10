54e773a408f35005315eef41e13cf3e9cc3af98f
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-11-10 16:34:16 +0700

    refactor: add breadcrumb components on PrivacyPolicy

--- src/views/app-views/landing-page/misc/PrivacyPolicy.jsx
+++ src/views/app-views/landing-page/misc/PrivacyPolicy.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import BannerDownload from "@/components/shared-components/BannerDownload";
-import { Breadcrumb } from 'antd';
-import { Link } from 'react-router-dom';
+import Breadcrumb from '@/components/layout-components/Breadcrumb';
 
 export default function PrivacyPolicy() {
     const privacy = [
@@ -59,24 +58,7 @@ export default function PrivacyPolicy() {
   return (
     <>
     <section className='base-container'>
-        <Breadcrumb 
-            className="pt-4 sm:pt-12"
-            separator={
-                <p className="-mt-[0.26em] text-2xl min-[991.98px]:-mt-1">»</p>
-            }
-            items={[
-                {
-                title: (
-                    <Link className="text-green-500" to="/">
-                    <p>Beranda</p>
-                    </Link>
-                ),
-                },
-                {
-                title: <p className="cursor-default">Kebijakan Privasi</p>,
-                },
-            ]}
-        />
+        <Breadcrumb currentPage="Kebijakan Privasi" />
     </section>
 
     <section className='base-container'>
