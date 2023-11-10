69b1bb8cd1eaab13eeed98954c5257666b72095b
aaaroz <ram.ardiansyah18@gmail.com>
2023-11-10 13:37:59 +0700

    refactor : change import url on public route using @

--- src/routers/PublicRoute.jsx
+++ src/routers/PublicRoute.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { LandingPageLayout } from '../components/LandingPageLayout'
+import { LandingPageLayout } from '@/components/LandingPageLayout'
 import { Outlet } from 'react-router-dom'
 
 export default function PublicRoute() {
