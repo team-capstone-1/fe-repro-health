38e0e8e8cf8bb9aacb07d2b12d08c54626f39f19
aaaroz <ram.ardiansyah18@gmail.com>
2023-11-11 16:29:32 +0700

    refactor (LandingPageLayout) : change import Navbar and Footer url

--- src/components/LandingPageLayout.jsx
+++ src/components/LandingPageLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "./layout-components/Navbar";
-import Footer from "./layout-components/Footer";
+import Navbar from "@/components/layout-components/Navbar";
+import Footer from "@/components/layout-components/Footer";
 import { Outlet } from "react-router-dom";
 
 function Content() {
