ea1db271165791b678e0d8a4a4f0fb3970b6a6f5
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-11-11 22:43:16 +0700

    refactor: (PrivacyPolicy): add constant folder and set constant privacy data

--- src/views/app-views/landing-page/misc/PrivacyPolicy.jsx
+++ src/views/app-views/landing-page/misc/PrivacyPolicy.jsx
@@ -1,60 +1,10 @@
 import React from 'react';
 import BannerDownload from "@/components/shared-components/BannerDownload";
 import Breadcrumb from '@/components/layout-components/Breadcrumb';
+import { privacy } from "@/views/app-views/landing-page/constant/privacy-policy"
 
 export default function PrivacyPolicy() {
-    const privacy = [
-        {
-            title: "Informasi Anda",
-            subs: ["Informasi yang dikumpulkan saat Anda menggunakan Platform, menerima atau menyediakan Layanan, meliputi:"],
-            textContent: [
-                "Informasi Pribadi Anda meliputi nama, alamat, nomor telepon, tanggal lahir, dan alamat email yang Anda berikan saat mendaftar di Platform kami. Informasi ini juga mencakup informasi pengidentifikasi pribadi lainnya yang Anda tambahkan ke akun, seperti biografi.",
-                "Informasi Kesehatan Pribadi Pelanggan meliputi jenis kelamin, kondisi kesehatan yang diketahui, pengobatan, alergi, vaksinasi, riwayat medis, target kesehatan sekaligus resep, laporan, hasil laboratorium, dan berkas medis yang diunggah oleh Pelanggan dari waktu ke waktu di Platform."
-            ]
-        },
-        {
-            title: "Tujuan Pengumpulan Data",
-            subs: ["Kami mengumpulkan informasi pribadi Anda untuk tujuan berikut:"],
-            textContent: [
-                "Memberikan layanan konsultasi dokter dan perawatan kesehatan yang sesuai.",
-                "Administrasi akun pengguna.",
-                "Komunikasi terkait perjanjian konsultasi dokter dan informasi lainnya yang relevan."
-            ]
-        },
-        {
-            title: "Keamanan Data",
-            subs: ["Kami mengambil langkah-langkah keamanan teknis dan organisasi yang sesuai untuk melindungi data pribadi Anda dari akses yang tidak sah, penggunaan, atau pengungkapan yang tidak sah. Data sensitif yang dikirimkan melalui website kami akan dienkripsi menggunakan protokol keamanan yang sesuai."],
-            textContent: []
-        },
-        {
-            title: "Hak Pengguna",
-            subs: ["Anda memiliki hak untuk:"],
-            textContent: [
-                "Mengakses dan memperbaiki informasi pribadi Anda.",
-                "Meminta penghapusan data pribadi Anda.",
-                "Meminta pembatasan penggunaan data pribadi Anda.",
-                "Menarik izin penggunaan data Anda."
-            ]
-        },
-        {
-            title: "Perubahan Kebijakan",
-            subs: ["Kebijakan privasi ini dapat diperbarui sesuai kebijakan yang berlaku. Setiap perubahan akan diumumkan di website kami. Penggunaan Platform secara berkelanjutan akan menandakan persetujuan dan penerimaan Anda atas perubahan-perubahan tersebut."],
-            textContent: []
-        },
-        {
-            title: "Kontak",
-            subs: ["Hukum privasi Indonesia memberikan hak bagi individu untuk mengakses, mengubah, dan menghapus informasi pribadi mereka. Jika Anda ingin mengubah atau menghapus informasi yang kami simpan mengenai Anda, silakan hubungi kami di bawah ini. Dan iika Anda memiliki pertanyaan atau kekhawatiran tentang kebijakan privasi ini, silakan hubungi kami melalui:"],
-            textContent: []
-        },
-        {
-            title: "Reprohealth Customer Care",
-            subs: [
-                "Email : support@reprohealth.com", 
-                "Nomor Telepon : 0821 3358 1616"
-            ],
-            textContent: []
-        }
-    ]
+    
   return (
     <>
     <section className='base-container'>
