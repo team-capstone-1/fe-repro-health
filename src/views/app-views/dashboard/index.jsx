1bf61e0b72e0d3ab5120006b92132b6a014b5b96
amorazl <alfhiyana11@gmail.com>
2023-11-13 15:23:08 +0700

    feat: add cards & appointment table

--- src/views/app-views/dashboard/index.jsx
+++ src/views/app-views/dashboard/index.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import { Row, Col } from "antd";
+
+import TotalCards from "./components/TotalCards";
+import AppointmentTable from "./components/AppointmentTable";
 
 export default function index() {
-  return <div>index</div>;
+  return (
+    <>
+      <div className="base-container">
+        <Row gutter={[8, 16]}>
+          <Col span={24}>
+            <TotalCards />
+          </Col>
+          <Col span={24}>
+            <AppointmentTable />
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
 }
