4f72dd81a3414ce4461aac0191f5011d28ac74a9
aaaroz <ram.ardiansyah18@gmail.com>
2023-11-13 22:33:57 +0700

    feat : add Topbar for layout-components

--- src/views/app-views/dashboard/index.jsx
+++ src/views/app-views/dashboard/index.jsx
@@ -1,11 +1,13 @@
 import { Row, Col } from "antd";
 
+import Topbar from "@/components/layout-components/Topbar";
 import TotalCards from "./components/TotalCards";
 import AppointmentTable from "./components/AppointmentTable";
 
 export default function index() {
   return (
     <>
+      <Topbar />
       <div className="base-container">
         <Row gutter={[8, 16]}>
           <Col span={24}>
