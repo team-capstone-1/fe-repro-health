ba2133a3cabf9732c3768efabc928cb390795e1e
aaaroz <ram.ardiansyah18@gmail.com>
2023-11-13 15:59:03 +0700

    refactor(login) : add some space on top and bottom of page

--- src/views/auth-views/Login.jsx
+++ src/views/auth-views/Login.jsx
@@ -38,7 +38,7 @@ const Login = () => {
     <section className="flex h-screen items-center justify-center xl:scale-90">
       <div className="base-container">
         <div className="flex items-center justify-center">
-          <div className="z-10 translate-x-0 rounded-lg bg-white px-8 py-8 shadow-none md:max-w-[655px] md:px-16 md:py-16 md:shadow-[2px_2px_4px_4px_rgba(186,186,186,0.3)] xl:-translate-x-[40%]">
+          <div className="z-10 translate-x-0 rounded-lg bg-white px-8 py-8 shadow-none md:max-w-[38rem] md:px-16 md:py-16 md:shadow-[2px_2px_4px_4px_rgba(186,186,186,0.3)] xl:-translate-x-[38%] xl:translate-y-[10%]">
             {/* Title */}
             <div className="space-y-1">
               <h2 className="font-semibold text-grey-900">
@@ -49,7 +49,7 @@ const Login = () => {
               </p>
             </div>
 
-            <form id="login-form" onSubmit={handleSubmit(onSubmitHandler)} className="mt-8">
+            <form className="mt-8">
               {/* Email */}
               <div>
                 <label className="text-base font-medium text-grey-300">
@@ -163,7 +163,7 @@ const Login = () => {
           </div>
 
           {/* Illustration */}
-          <div className="absolute hidden translate-x-[40%] xl:block">
+          <div className="absolute hidden translate-x-[40%] translate-y-[8%] py-5 xl:block">
             <img
               className="-z-10 w-[90%]"
               src={loginIllus}
