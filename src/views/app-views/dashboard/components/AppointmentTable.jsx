a41b6359bd40858201e8196aeb016da3d8d1ffa5
amorazl <alfhiyana11@gmail.com>
2023-11-14 14:57:44 +0700

    feat: add AppointmentTable & ButtonFilter

--- src/views/app-views/dashboard/components/AppointmentTable.jsx
+++ src/views/app-views/dashboard/components/AppointmentTable.jsx
@@ -1,11 +1,11 @@
-import { Row, Col, Card, Table } from "antd";
+import { Row, Col, Card, Table, Button } from "antd";
 
 export default function AppointmentTable() {
   const columns = [
     {
-      title: "No Urut",
-      dataIndex: "no",
-      key: "no",
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
     },
     {
       title: "Nama Pasien",
@@ -14,30 +14,74 @@ export default function AppointmentTable() {
       render: (text) => <a>{text}</a>,
     },
     {
+      title: "No Urut",
+      dataIndex: "no",
+      key: "no",
+    },
+    {
       title: "Tanggal",
       dataIndex: "date",
       key: "date",
     },
     {
-      title: "Biaya",
-      dataIndex: "cost",
-      key: "cost",
+      title: "Sesi",
+      dataIndex: "session",
+      key: "session",
     },
     {
       title: "Status",
       dataIndex: "status",
       key: "status",
+      render: (_, { status }) => (
+        <>
+          {status.map((tag) => {
+            let color;
+            if (tag === "Berjalan") {
+              color = "text-link bg-link-25";
+            } else if (tag === "Menunggu") {
+              color = "text-warning bg-warning-25";
+            } else {
+              color = "text-negative bg-negative-25";
+            }
+
+            return (
+              <Button className={color} key={tag} type="primary">
+                <span className="font-medium">{tag}</span>
+              </Button>
+            );
+          })}
+        </>
+      ),
     },
   ];
+
   const data = [
     {
-      key: "1",
-      no: "009",
+      id: "#001",
       name: "Naufal Helmi",
-      date: "23/10/23 - Pagi",
-      cost: "Rp 123.456",
+      no: "009",
+      date: "23/10/23",
+      session: "Pagi",
+      status: ["Menunggu"],
+    },
+    {
+      id: "#001",
+      name: "Naufal Helmi",
+      no: "009",
+      date: "23/10/23",
+      session: "Pagi",
+      status: ["Berjalan"],
+    },
+    {
+      id: "#001",
+      name: "Naufal Helmi",
+      no: "009",
+      date: "23/10/23",
+      session: "Pagi",
+      status: ["Dibatalkan"],
     },
   ];
+
   return (
     <>
       <Row gutter={[16]}>
--- src/views/app-views/dashboard/index.jsx
+++ src/views/app-views/dashboard/index.jsx
@@ -1,18 +1,20 @@
 import { Row, Col, Layout } from "antd";
 
 import Topbar from "@/components/layout-components/Topbar";
+import Sidebar from "./components/Sidebar";
+import ButtonFilter from "./components/ButtonFilter";
 import TotalCards from "./components/TotalCards";
 import AppointmentTable from "./components/AppointmentTable";
-import Sidebar from "./components/Sidebar";
 
-export default function index() {
+export default function Dashboard() {
   return (
     <>
       <Layout className="lg:h-screen">
         <Topbar />
         <main className="flex">
           <Sidebar />
-          <div className="px-12">
+          <div className="px-5">
+            <ButtonFilter />
             <Row gutter={[8, 16]}>
               <Col span={24}>
                 <TotalCards />
