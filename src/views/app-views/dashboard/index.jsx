30b29c9ceb3fd59afa66732ea7cfaae91af09a1a
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-11-15 01:08:04 +0700

    feat: add component ChartIncome for dashboard page and install library Rechart

--- src/views/app-views/dashboard/index.jsx
+++ src/views/app-views/dashboard/index.jsx
@@ -4,6 +4,7 @@ import ButtonFilter from "./components/ButtonFilter";
 import TotalCards from "./components/TotalCards";
 import AppointmentTable from "./components/AppointmentTable";
 import Calendar from "./components/Calendar";
+import ChartIncome from "./components/ChartIncome";
 
 export default function Dashboard() {
   return (
@@ -12,6 +13,9 @@ export default function Dashboard() {
       <Row gutter={[8, 16]}>
         <Col span={24}>
           <TotalCards />
+        </Col>
+        <Col span={24}>
+          <ChartIncome />
         </Col>
         <Col span={24}>
           <AppointmentTable />
