924c14db2085081882022d5972134c710af0bfbf
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-11-15 19:46:01 +0700

    refactor: (ChartIncome) add utils DataIncome

--- src/views/app-views/dashboard/components/ChartIncome.jsx
+++ src/views/app-views/dashboard/components/ChartIncome.jsx
@@ -8,54 +8,9 @@ import {
   Tooltip,
   ResponsiveContainer as Wrapper,
   Legend,
-  LabelList,
 } from "recharts";
 
-// dataset
-const data = [
-  {
-    year: "Januari",
-    after: 53000000,
-    // prev: 20,
-    amount: 0,
-  },
-  {
-    year: "Februari",
-    after: 43000000,
-    // prev: 20,
-    amount: 10000000,
-  },
-  {
-    year: "Maret",
-    after: 33000000,
-    // prev: 20,
-    amount: 20000000,
-  },
-  {
-    year: "April",
-    after: 50000000,
-    // prev: 20,
-    amount: 30000000,
-  },
-  {
-    year: "Mei",
-    after: 47000000,
-    // prev: 20,
-    amount: 40000000,
-  },
-  {
-    year: "Juni",
-    after: 45000000,
-    // prev: 20,
-    amount: 50000000,
-  },
-  {
-    year: "Juli",
-    after: 49000000,
-    // prev: 20,
-    amount: 60000000,
-  },
-];
+import { DataIncome as data } from "@/utils/DataIncome";
 
 const CustomTooltip = ({ active, payload, label }) => {
   if (active && payload && payload.length) {
