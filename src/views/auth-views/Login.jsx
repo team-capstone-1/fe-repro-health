cb5c473438d7fcb6da05638e07652b65665cca9d
BagusAri-17 <bagusari.widhi@gmail.com>
2023-11-15 19:56:07 +0800

    refactor: add validate in login page

--- src/views/auth-views/Login.jsx
+++ src/views/auth-views/Login.jsx
@@ -1,13 +1,40 @@
 import { useState } from "react";
 import loginIllus from "@/assets/login-illustration.svg";
 import { AiOutlineMail, AiOutlineLock } from "react-icons/ai";
+import { MdError } from "react-icons/md";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const Login = () => {
   const [isFocusEmail, setIsFocusEmail] = useState(false);
   const [isFocusPass, setIsFocusPass] = useState(false);
 
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .required("Email harus diisi")
+      .email("Email harus sesuai")
+      .oneOf(["dummy@example.com"], "Email tidak valid"),
+    password: yup
+      .string()
+      .required("Kata sandi harus diisi")
+      .min(8, "Kata sandi minimal 8 karakter")
+      .oneOf(["helloworld"], "Kata sandi tidak valid"),
+    // .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/, "Password")
+  });
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmitHandler = (data) => console.log(data);
+
   return (
-    <section className="flex h-screen items-center justify-center xl:scale-95">
+    <section className="flex h-screen items-center justify-center xl:scale-90">
       <div className="base-container">
         <div className="flex items-center justify-center">
           <div className="z-10 translate-x-0 rounded-lg bg-white px-8 py-8 shadow-none md:max-w-[38rem] md:px-14 md:py-14 md:shadow-[2px_2px_4px_4px_rgba(186,186,186,0.3)] xl:-translate-x-[38%]">
@@ -21,7 +48,7 @@ const Login = () => {
               </p>
             </div>
 
-            <form className="mt-8">
+            <form onSubmit={handleSubmit(onSubmitHandler)} className="mt-8">
               {/* Email */}
               <div>
                 <label className="text-base font-medium text-grey-300">
@@ -31,43 +58,101 @@ const Login = () => {
                   <div className="absolute inset-y-0 start-0 flex items-center ps-4">
                     <AiOutlineMail
                       color={`${
-                        isFocusEmail === false ? "#b9b9b9" : "#0d0d0d"
+                        isFocusEmail
+                          ? "#0d0d0d"
+                          : errors.email
+                          ? "#fc4547"
+                          : "#b9b9b9"
                       }`}
                       size={24}
                     />
                   </div>
                   <input
+                    {...register("email")}
                     onFocus={() => setIsFocusEmail(true)}
                     onBlur={() => setIsFocusEmail(false)}
                     id="email"
                     type="email"
-                    className="block w-full rounded-lg border border-grey-100 p-4 ps-14 text-base text-grey-100 focus:border-grey-900 focus:text-grey-900 focus:outline-none focus:ring-1 focus:ring-grey-900"
+                    className={`block w-full rounded-lg border border-grey-100 p-4 pe-8 ps-14 text-base focus:border-grey-900 focus:text-grey-900 focus:outline-none focus:ring-1 focus:ring-grey-900 ${
+                      errors.email
+                        ? "border-[#fc4547] text-[#fc4547]"
+                        : "border-grey-100 text-grey-100"
+                    }`}
                     placeholder="Enter your email address"
                   />
+                  <div
+                    className={`absolute inset-y-0 end-0 items-center pe-4 ${
+                      errors.email ? "flex" : "hidden"
+                    }`}
+                  >
+                    <MdError
+                      color={`${
+                        isFocusEmail
+                          ? "#0d0d0d"
+                          : errors.email
+                          ? "#fc4547"
+                          : "#b9b9b9"
+                      }`}
+                      size={24}
+                    />
+                  </div>
                 </div>
+                <span className=" text-xs text-[#fc4547]">
+                  {errors.email?.message}
+                </span>
               </div>
 
               {/* Passoword */}
               <div className="mt-2">
                 <label className="text-base font-medium text-grey-300">
-                  Password
+                  Kata Sandi
                 </label>
                 <div className="relative mt-2">
                   <div className="absolute inset-y-0 start-0 flex items-center ps-4">
                     <AiOutlineLock
-                      color={`${isFocusPass === false ? "#b9b9b9" : "#0d0d0d"}`}
+                      color={`${
+                        isFocusPass
+                          ? "0d0d0d"
+                          : errors.password
+                          ? "#fc4547"
+                          : "#b9b9b9"
+                      }`}
                       size={24}
                     />
                   </div>
                   <input
+                    {...register("password")}
                     onFocus={() => setIsFocusPass(true)}
                     onBlur={() => setIsFocusPass(false)}
                     id="password"
                     type="password"
-                    className="block w-full rounded-lg border border-grey-100 p-4 ps-14 text-base text-grey-100 focus:border-grey-900 focus:text-grey-900 focus:outline-none focus:ring-1 focus:ring-grey-900"
+                    className={`block w-full rounded-lg border p-4 ps-14 text-base focus:border-grey-900 focus:text-grey-900 focus:outline-none focus:ring-1 focus:ring-grey-900 ${
+                      errors.password
+                        ? "border-[#fc4547] text-[#fc4547]"
+                        : "border-grey-100 text-grey-100"
+                    }`}
                     placeholder="Enter your password"
                   />
+                  <div
+                    className={`absolute inset-y-0 end-0 items-center pe-4 ${
+                      errors.password ? "flex" : "hidden"
+                    }`}
+                  >
+                    <MdError
+                      color={`${
+                        isFocusPass
+                          ? "#0d0d0d"
+                          : errors.password
+                          ? "#fc4547"
+                          : "#b9b9b9"
+                      }`}
+                      size={24}
+                    />
+                  </div>
                 </div>
+                <span className=" text-xs text-red-500">
+                  {errors.password?.message}
+                </span>
               </div>
 
               {/* Remember me & Forgot Password */}
@@ -82,7 +167,7 @@ const Login = () => {
                       htmlFor=""
                       className="text-base font-medium text-grey-200"
                     >
-                      Remember me
+                      Ingatkan saya
                     </label>
                   </div>
                 </div>
@@ -90,7 +175,7 @@ const Login = () => {
                   href="/forgot-password"
                   className="text-base font-medium text-green-500 hover:text-green-600"
                 >
-                  Forgot Password?
+                  Lupa Password?
                 </a>
               </div>
 
@@ -106,7 +191,7 @@ const Login = () => {
           {/* Illustration */}
           <div className="absolute hidden translate-x-[40%] py-5 xl:block">
             <img
-              className="-z-10 w-[82%]"
+              className="-z-10 w-[85%]"
               src={loginIllus}
               alt="login-illustration"
             />
