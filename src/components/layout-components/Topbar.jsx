a1d35bfeb47432b008392a2770e67855f8605dd9
aaaroz <ram.ardiansyah18@gmail.com>
2023-11-15 16:20:40 +0700

    refactor : add prefix size on top bar

--- src/components/layout-components/Topbar.jsx
+++ src/components/layout-components/Topbar.jsx
@@ -6,28 +6,30 @@ import logoReproHealth from "@/assets/logo-reprohealth.png";
 
 export default function Topbar() {
   return (
-    <nav className="sticky left-0 top-0 z-50 w-full border bg-white">
+    <nav className="sticky left-0 top-0 z-50 w-full border bg-grey-10">
       <div className="app-container z-10 flex justify-between py-5 md:py-3">
         <div className="flex items-center">
           <Link to="/">
             <img
               src={logoReproHealth}
               alt="logo"
-              className="w-40 cursor-pointer"
+              className="w-28 cursor-pointer md:w-40"
             />
           </Link>
         </div>
 
-        <div className="flex items-center gap-3">
-          <div className="text-2xl">
+        <div className="flex items-center gap-1 sm:gap-3">
+          <div className="text-xl md:text-2xl">
             <IoNotificationsOutline />
           </div>
-          <div className="h-11 w-11 rounded-full bg-slate-700">
+          <div className="h-8 w-8 rounded-full bg-slate-700 md:h-11 md:w-11">
             <img src={profileDoctor} alt="profile-doctor" />
           </div>
           <div>
-            <h6 className="text-base leading-none">Dr. Andi Cahaya, Sp.OG</h6>
-            <span className="text-sm font-medium leading-none">
+            <h6 className="text-xs leading-none sm:text-sm md:text-base">
+              Dr. Andi Cahaya, Sp.OG
+            </h6>
+            <span className="text-xs font-medium leading-none md:text-sm">
               Spesialis Kandungan
             </span>
           </div>
