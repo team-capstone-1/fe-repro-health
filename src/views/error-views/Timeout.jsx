5ca89a15341a9fadd6fea6a6216285eb94eba226
aaaroz <ram.ardiansyah18@gmail.com>
2023-11-16 00:51:34 +0700

    refactor: add bg-image for timeout page

--- src/views/error-views/Timeout.jsx
+++ src/views/error-views/Timeout.jsx
@@ -1,10 +1,5 @@
-import { VscDebugDisconnect } from "react-icons/vsc";
-
 export default function Timeout() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-vector-header text-center">
-      {/* <VscDebugDisconnect className="text-4xl" /> */}
-      <h5>Disconnected</h5>
-    </div>
+    <div className="bg-error-timeout flex h-screen w-screen items-center justify-center bg-center bg-no-repeat text-center"></div>
   );
 }
