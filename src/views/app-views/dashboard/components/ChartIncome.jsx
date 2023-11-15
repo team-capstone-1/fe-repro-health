0105e57fa2d1d71b63685547c1c2a7020ad424ab
Naufal Nasrullah <naufaln176@gmail.com>
2023-11-15 21:52:09 +0700

    style: adjust some layout styling

--- src/views/app-views/dashboard/components/ChartIncome.jsx
+++ src/views/app-views/dashboard/components/ChartIncome.jsx
@@ -1,5 +1,4 @@
-import { Card, Col, DatePicker, Row, Space } from "antd";
-import dayjs from "dayjs";
+import { Card, Col, Row } from "antd";
 import {
   BarChart,
   Bar,
@@ -7,30 +6,25 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  ResponsiveContainer,
-  //   LabelList,
+  ResponsiveContainer as Wrapper,
+  Legend,
 } from "recharts";
 
-import {
-  DataIncome as data,
-  RangePresets as rangePresets,
-} from "@/views/app-views/dashboard/constant/graph-income";
-
-import { useState } from "react";
+import { DataIncome as data } from "@/utils/DataIncome";
 
 const CustomTooltip = ({ active, payload, label }) => {
   if (active && payload && payload.length) {
     return (
-      <div className="w-full rounded-[4px] bg-white px-3 py-3 shadow-lg">
-        <p id="label-year" className="mb-4 text-base text-black">
-          {`${label} `}
-        </p>
-
-        <p className="text-base text-black">
-          {`${payload[0].value.toLocaleString("id-ID", {
+      <div className="w-full rounded-lg bg-slate-50 px-3 py-3">
+        <p className="mb-4 text-base text-neutral-600">
+          {`${label} : Rp. ${payload[0].value.toLocaleString("id-ID", {
             minimumFractionDigits: 0,
             maximumFractionDigits: 0,
-          })} Rupiah`}
+          })}`}
+        </p>
+
+        <p className="text-base text-neutral-600">
+          {`Pendapatan Bulan ${label}.`}
         </p>
       </div>
     );
@@ -40,100 +34,43 @@ const CustomTooltip = ({ active, payload, label }) => {
 };
 
 export default function ChartIncome() {
-  // const customTickYAxis = (values) => `${values.toString().slice(0, 2)} jt`;
-  const customTickYAxis = (values) => {
-    if (values > 99000000) {
-      return `${values.toString().slice(0, 3)} jt`;
-    } else {
-      return `${values.toString().slice(0, 2)} jt`;
-    }
-  };
+  const customTickYAxis = (values) => `${values.toString().slice(0, 2)} jt`;
   const customTickXAxis = (value) => value.slice(0, 3);
 
-  const mobileSize = window.innerWidth >= 450;
-
-  const { RangePicker } = DatePicker;
-
-  const [selectedRange, setSelectedRange] = useState("");
-  const [startDate, setStartDate] = useState(selectedRange[0]);
-  const [endDate, setEndDate] = useState(selectedRange[1]);
-  const dateFormat = "DD/MM/YYYY";
-
-  const onRangeChange = (dates) => {
-    if (dates) {
-      setStartDate(dates[0]);
-      setEndDate(dates[1]);
-      setSelectedRange(dates);
-      console.log(dates);
-    } else {
-      setStartDate("");
-      setEndDate("");
-      setSelectedRange("");
-    }
-  };
-
-  const getLabel = () => {
-    if (startDate && endDate) {
-      const formattedStartDate = dayjs(startDate).format("DD MMMM YYYY");
-      const formattedEndDate = dayjs(endDate).format("DD MMMM YYYY");
-      return `Pendapatan dari ${formattedStartDate} hingga ${formattedEndDate}`;
-    } else {
-      return `Pilih Rentang Pendapatan...`;
-    }
-  };
-
   return (
-    <Row id="chart-income" justify="start">
+    <Row justify="start">
       <Col span={24} xs={24} md={24} lg={24}>
         <Card>
-          <section>
-            <h3 id="title-graph" className="text-2xl font-semibold text-black">
+          <div className="pl-4">
+            <h3 className="text-2xl font-semibold text-black">
               Grafik Pendapatan
             </h3>
-            <h6 id="label-graph" className="my-2">
-              {getLabel()}
-            </h6>
+            <h6>Menampilkan data 7 bulan terakhir</h6>
+          </div>
 
-            <Col span={24}>
-              <Space direction="vertical" size={12}>
-                <RangePicker
-                  id="range-picker-chart-income"
-                  presets={rangePresets}
-                  onChange={onRangeChange}
-                  format={dateFormat}
-                />
-              </Space>
-            </Col>
-          </section>
-
-          <Wrapper id="chart-income-wrapper" width="100%" height={340}>
+          <Wrapper width="100%" height={385}>
             <BarChart
-              id="bar-chart"
               // width={770}
               // height={408}
               data={data}
-              barGap={0}
+              barGap={5}
               margin={{
-                top: 15,
+                top: 5,
                 right: 0,
-                left: 0,
+                left: 10,
                 bottom: 10,
               }}
-              // maxBarSize={30}
             >
               <Legend
-                id="legend-chart-income"
                 verticalAlign="top"
                 align="end"
                 iconSize={20}
                 iconType="circle"
                 formatter={(value) => (
-                  <span className="text-xs text-black md:text-[14px] lg:text-[16px]">
-                    {value}
-                  </span>
+                  <span className="text-black">{value}</span>
                 )}
                 wrapperStyle={{
-                  paddingBottom: "10px",
+                  paddingBottom: "50px",
                 }}
               />
 
@@ -142,47 +79,51 @@ export default function ChartIncome() {
               <XAxis
                 tickFormatter={customTickXAxis}
                 orientation="bottom"
-                dataKey="date"
+                dataKey="year"
                 tickLine={false}
                 axisLine={false}
-                stroke="rgba(75, 75, 75, 1)"
+                tickMargin={20}
                 className="text-base font-medium"
               />
 
               <YAxis
                 tickFormatter={customTickYAxis}
                 orientation="left"
-                type="number"
-                // dataKey="amount"
+                type="string"
+                dataKey="amount"
                 tickLine={false}
                 axisLine={false}
-                tickMargin={10}
-                // ticks={[0, 10, 20, 30, 40, 50, 60]}
-                custom
-                show
-                tick
+                tickMargin={20}
+                // ticks={[0, 10, 20, 30, 40, 50, 60]} custom show tick
                 className="text-base"
               />
 
               <Tooltip
-                id="tooltip-chart-income"
                 cursor={{ fill: "transparent" }}
                 content={CustomTooltip}
               />
 
+              {/* <Bar
+                barSize={30}
+                radius={10}
+                dataKey="prev"
+                fill="#8896AB"
+                name="Pendapatan Sebelum"
+              >
+                <LabelList position="insideTop" fill="white" />
+              </Bar> */}
+
               <Bar
-                id="bar-chart-income"
-                barSize={mobileSize || data.length < 20 ? 30 : 5}
-                // barSize={data.length < 8 ? 30 : 20}
-                radius={data.length < 10 ? 10 : 2}
+                barSize={30}
+                radius={10}
                 dataKey="after"
-                name="Pekan Kini"
+                name="Jumlah Pendapatan"
                 fill="rgba(20, 198, 164, 1)"
               >
                 {/* <LabelList position="top" fill="black" /> */}
               </Bar>
             </BarChart>
-          </ResponsiveContainer>
+          </Wrapper>
         </Card>
       </Col>
     </Row>
