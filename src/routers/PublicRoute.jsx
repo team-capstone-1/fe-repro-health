bce92c655e937cf0160f07a3cbbdea0d16b6193d
aaaroz <ram.ardiansyah18@gmail.com>
2023-11-16 00:17:51 +0700

    feat: add error timeout on landing-page

--- src/routers/PublicRoute.jsx
+++ src/routers/PublicRoute.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useSyncExternalStore } from "react";
 import { LandingPageLayout } from "@/components/layout-components/LandingPageLayout";
 import { Outlet } from "react-router-dom";
+import Timeout from "@/views/error-views/Timeout";
 
 export default function PublicRoute() {
-  return (
-    <LandingPageLayout>
-      <Outlet />
-    </LandingPageLayout>
-  );
+  const isOnline = useSyncExternalStore(subscribe, getSnapshot);
+
+  if (isOnline) {
+    return (
+      <LandingPageLayout>
+        <Outlet />
+      </LandingPageLayout>
+    );
+  } else {
+    return <Timeout />;
+  }
+}
+function getSnapshot() {
+  return navigator.onLine;
+}
+function subscribe(callback) {
+  window.addEventListener("online", callback);
+  window.addEventListener("offline", callback);
+  return () => {
+    window.removeEventListener("online", callback);
+    window.removeEventListener("offline", callback);
+  };
 }
---
+++ src/views/error-views/Timeout.jsx
@@ -0,0 +1,3 @@
+export default function Timeout() {
+  return <div>Timeout</div>;
+}
