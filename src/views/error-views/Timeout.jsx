3d71e1434923776e567dfeef43bb0f39a24becd5
aaaroz <ram.ardiansyah18@gmail.com>
2023-11-16 00:48:36 +0700

    feat: background image, error-timeout

--- src/views/error-views/Timeout.jsx
+++ src/views/error-views/Timeout.jsx
@@ -1,3 +1,10 @@
+import { VscDebugDisconnect } from "react-icons/vsc";
+
 export default function Timeout() {
-  return <div>Timeout</div>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-vector-header text-center">
+      {/* <VscDebugDisconnect className="text-4xl" /> */}
+      <h5>Disconnected</h5>
+    </div>
+  );
 }
