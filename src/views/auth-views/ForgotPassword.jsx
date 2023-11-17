ab2fd065c648dece10becd22fae2e0e0701b2db9
BagusAri-17 <bagusari.widhi@gmail.com>
2023-11-17 13:51:37 +0800

    refactor: add unique id property on each components

--- src/views/auth-views/ForgotPassword.jsx
+++ src/views/auth-views/ForgotPassword.jsx
@@ -38,12 +38,12 @@ const ForgotPassword = () => {
     <section className="flex items-center justify-center h-screen xl:scale-95">
       <div className="base-container">
         <div className="mx-auto max-w-[1200px] rounded-lg bg-white p-8 shadow-none md:p-14 md:shadow-[2px_2px_4px_4px_rgba(186,186,186,0.3)]">
-          <button onClick={() => navigate(-1)} className="flex gap-3">
+          <button id="back-button" onClick={() => navigate(-1)} className="flex gap-3">
             <AiOutlineArrowLeft color="#989898" size={24} />
             <p className="text-base font-semibold text-grey-900">Kembali</p>
           </button>
           <div className="mt-[26px] grid grid-cols-1 items-center gap-24 lg:grid-cols-2">
-            <form className="flex flex-col gap-12 lg:gap-20">
+            <form id="forgot-password-form" className="flex flex-col gap-12 lg:gap-20">
               {/* Title */}
               <div>
                 <h3 className="text-grey-900">Lupa Kata Sandi Anda</h3>
