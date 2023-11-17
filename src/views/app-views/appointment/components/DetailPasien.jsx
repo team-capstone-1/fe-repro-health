bdfc07d5c1b4df78486808e2ad9fa5bf6aa99fe1
aaaroz <ram.ardiansyah18@gmail.com>
2023-11-18 00:06:59 +0700

    feat: add Detail Pasien Components on appointment page and set link to appointment page

---
+++ src/views/app-views/appointment/components/DetailPasien.jsx
@@ -0,0 +1,159 @@
+import { Button, Card, Drawer, Flex } from "antd";
+import { DataPasien } from "@/views/app-views/appointment/constant/detail-pasien";
+
+export default function DetailPasien({ handleOpen, isOpen }) {
+  return (
+    <Drawer
+      width={500}
+      title={<HeaderDrawer />}
+      placement="right"
+      onClose={handleOpen}
+      open={isOpen}
+    >
+      <ContentDrawer />
+    </Drawer>
+  );
+}
+
+const HeaderDrawer = () => {
+  return (
+    <div className="ps-5">
+      <h5 className="text-xl font-medium">Detail Pasien</h5>
+    </div>
+  );
+};
+
+const ContentDrawer = () => {
+  return (
+    <>
+      <Card id="card-pasien-detail" className="mt-0">
+        <Flex justify="center">
+          <img
+            src={DataPasien.image}
+            alt="pasien"
+            className="h-[6.25rem] w-[6.25rem] rounded-full border bg-white"
+          />
+        </Flex>
+        <Flex justify="center" className="mt-2">
+          <h5 className="font-medium">{DataPasien.name}</h5>
+        </Flex>
+        {DataPasien.detail.map((item, i) => (
+          <Flex justify="space-between" className="mt-2" key={i}>
+            <p className="text-xs font-normal sm:text-sm">
+              {Object.keys(item)}
+            </p>
+            <p className="text-xs font-medium sm:text-sm">
+              {Object.values(item)}
+            </p>
+          </Flex>
+        ))}
+      </Card>
+
+      <Card id="card-informasi-janji-temu" className="mt-4">
+        <Flex>
+          <h5 className="font-medium">{DataPasien.appointment}</h5>
+        </Flex>
+        {DataPasien.appointmentDetail.map((item, i) => (
+          <Flex justify="space-between" className="mt-2" key={i}>
+            {item["Metode Pembayaran"] ? (
+              <>
+                <p className="text-xs font-normal sm:text-sm">
+                  {Object.keys(item)}
+                </p>
+                <p className="flex flex-col items-end text-xs font-medium sm:text-sm">
+                  {Object.values(item)}
+                  <span className="text-xs text-positive">
+                    Lihat Bukti Transfer
+                  </span>
+                </p>
+              </>
+            ) : (
+              <>
+                <p className="text-xs font-normal sm:text-sm">
+                  {Object.keys(item)}
+                </p>
+                <p className="text-xs font-medium sm:text-sm">
+                  {Object.values(item)}
+                </p>
+              </>
+            )}
+          </Flex>
+        ))}
+        {DataPasien.status === "Menunggu" && (
+          <>
+            <Flex justify="space-between" className="mt-2">
+              <p className="text-xs font-normal sm:text-sm">Status</p>
+              <p className="cursor-default rounded-lg bg-warning-25 px-6 py-2 text-xs font-medium text-warning">
+                {DataPasien.status}
+              </p>
+            </Flex>
+            <Flex justify="center" className="mt-5">
+              <Button
+                type="primary"
+                className="bg-green-500 px-10 pb-8 pt-2 hover:bg-green-600 disabled:bg-grey-100 disabled:text-grey-200"
+              >
+                Konfirmasi Sekarang
+              </Button>
+            </Flex>
+          </>
+        )}
+        {DataPasien.status === "Berjalan" && (
+          <>
+            <Flex justify="space-between" className="mt-2">
+              <p className="text-xs font-normal sm:text-sm">Status</p>
+              <p className="cursor-default rounded-lg bg-link-25 px-6 py-2 text-xs font-medium text-link">
+                {DataPasien.status}
+              </p>
+            </Flex>
+            <Flex justify="center" className="mt-5">
+              <Button
+                type="primary"
+                className="bg-green-500 px-10 pb-8 pt-2 hover:bg-green-600 disabled:bg-grey-100 disabled:text-grey-200"
+              >
+                Selesaikan Sekarang
+              </Button>
+            </Flex>
+          </>
+        )}
+        {DataPasien.status === "Dibatalkan" && (
+          <>
+            <Flex justify="space-between" className="mt-2">
+              <p className="text-xs font-normal sm:text-sm">Status</p>
+              <p className="cursor-default rounded-lg bg-negative-25 px-6 py-2 text-xs font-medium text-negative">
+                {DataPasien.status}
+              </p>
+            </Flex>
+            <Flex justify="center" className="mt-5">
+              <Button
+                type="primary"
+                className="bg-green-500 px-10 pb-8 pt-2 hover:bg-green-600 disabled:bg-grey-100 disabled:text-grey-200"
+                disabled
+              >
+                Selesaikan Sekarang
+              </Button>
+            </Flex>
+          </>
+        )}
+        {DataPasien.status === "Selesai" && (
+          <>
+            <Flex justify="space-between" className="mt-2">
+              <p className="text-xs font-normal sm:text-sm">Status</p>
+              <p className="cursor-default rounded-lg bg-positive-25 px-6 py-2 text-xs font-medium text-positive">
+                {DataPasien.status}
+              </p>
+            </Flex>
+            <Flex justify="center" className="mt-5">
+              <Button
+                type="primary"
+                className="bg-green-500 px-10 pb-8 pt-2 hover:bg-green-600 disabled:bg-grey-100 disabled:text-grey-200"
+                disabled
+              >
+                Selesaikan Sekarang
+              </Button>
+            </Flex>
+          </>
+        )}
+      </Card>
+    </>
+  );
+};
