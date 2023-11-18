87f0a1c24caf9d10a4715c2894ee5ad6db8b018c
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-11-18 22:13:59 +0700

    add layout and tabs doctor profile

--- src/views/app-views/profile/components/TabsDoctor.jsx
+++ src/views/app-views/profile/components/TabsDoctor.jsx
@@ -1,10 +1,8 @@
-import { Card, ConfigProvider, Tabs } from "antd";
-
+import { Card, Col, Row, Tabs } from "antd";
+import React from "react";
 import WorkExperience from "./WorkExperience";
-import Education from "./Education";
-import Certificate from "./Certificate";
 
-export default function TabsDoctor() {
+function TabsDoctor() {
   const onChange = (key) => {
     console.log(key);
   };
@@ -12,37 +10,26 @@ export default function TabsDoctor() {
   const tabs = [
     {
       key: "1",
-      label: <p className="w-[420px] text-center">Pengalaman kerja</p>,
+      label: <p className="w-[430px] text-center">Pengalaman kerja</p>,
       children: <WorkExperience />,
     },
     {
       key: "2",
-      label: <p className="w-[420px] text-center">Pendidikan</p>,
-      children: <Education />,
+      label: <p className="w-[430px] text-center">Pendidikan</p>,
+      children: "Content of Tab Pane 2",
     },
     {
       key: "3",
-      label: <p className="w-[420px] text-center">Sertifikasi</p>,
-      children: <Certificate />,
+      label: <p className="w-[430px] text-center">Sertifikasi</p>,
+      children: "Content of Tab Pane 3",
     },
   ];
 
   return (
-    <section id="tabs-profile">
-      <ConfigProvider
-        theme={{
-          components: {
-            Tabs: {},
-          },
-          token: {
-            colorPrimary: "#14c6a4",
-          },
-        }}
-      >
-        <Card>
-          <Tabs defaultActiveKey="3" items={tabs} onChange={onChange} />
-        </Card>
-      </ConfigProvider>
-    </section>
+    <Card>
+      <Tabs defaultActiveKey="1" items={tabs} onChange={onChange} />
+    </Card>
   );
 }
+
+export default TabsDoctor;
