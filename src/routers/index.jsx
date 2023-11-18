33efb95e900ad52ad33e34f3d7e4a0b364973229
Aurelia Amanda Robert <aurelia.amanda@ui.ac.id>
2023-11-18 16:31:37 +0800

    fix: Router for Profile Page

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -35,7 +35,7 @@ export default function SetupRoutes() {
         <Route path="/forum/:questionId" element={<DiscussionDetail />} />
         <Route path="/profil" element={<Profile />} />
         <Route path="/forum" element={<Forum />} />
-        <Route path="/profil" element={<DoctorProfile />} />
+        <Route path="/profil" element={<Profile />} />
       </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
