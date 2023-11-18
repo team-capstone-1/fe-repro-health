3c44a468f35590e2f66b251b2f2ce76ba714dbb2
Naufal Nasrullah <naufaln176@gmail.com>
2023-11-18 18:33:27 +0700

    feat(forum): complete forum page with mock API

--- src/views/app-views/forum/index.jsx
+++ src/views/app-views/forum/index.jsx
@@ -1,28 +1,13 @@
 import { Tabs } from "@/components/shared-components/Tabs";
-import { Input } from "antd";
-import { useState } from "react";
-const { Search } = Input;
+import BelumDijawab from "./components/BelumDiJawab";
+import SemuaDiskusi from "./components/SemuaDiskusi";
 
 export default function Forum() {
-  const [isLoading, setIsLoading] = useState(false);
   return (
     <>
-      <Tabs>
-        {/* isi content tab 1 */}
-        <div className="mt-4">
-          <Search
-            className="rounded-lg bg-blue-400"
-            placeholder="input search text"
-            enterButton="Search"
-            size="large"
-            allowClear
-            loading={isLoading}
-            onSearch={(prev) => setIsLoading(!prev)}
-          />
-        </div>
-
-        {/* isi content tab 1 */}
-        <div className="mt-4">Tab 2</div>
+      <Tabs title={["Semua Diskusi", "Belum Dijawab"]}>
+        <SemuaDiskusi />
+        <BelumDijawab />
       </Tabs>
     </>
   );
