d835264501f62e7fad06379675f689ea2fcc0d08
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-11-19 19:51:02 +0700

    refactor: (ProfileDoctor) fixing some style, add constant data

--- src/views/app-views/profile/components/Education.jsx
+++ src/views/app-views/profile/components/Education.jsx
@@ -1,15 +1,66 @@
 import { ConfigProvider, Timeline } from "antd";
-import { DataEducation as education } from "../constant/data-education";
+import { DataEducations as educations } from "../constant/data-education";
+import { useState } from "react";
 
 export default function Education() {
+  const CustomDot = ({ onClick, selected }) => (
+    <button
+      onClick={onClick}
+      style={{
+        width: 16,
+        height: 16,
+        border: "2px solid #14C6A4",
+        borderRadius: "100%",
+        backgroundColor: selected ? "#14C6A4" : "transparent",
+      }}
+    />
+  );
+
+  const [selectedDot, setSelectedDot] = useState(0);
+
+  const handleDotClick = (index) => {
+    setSelectedDot(index);
+  };
+
+  const educationItems = educations.map((education, index) => ({
+    label: (
+      <p className="me-[90px] font-semibold ">
+        {education.starting_date} - {education.end_date}
+      </p>
+    ),
+
+    children: (
+      <div className="ms-[20px]">
+        <ul key={education.id} className="w-[500px]">
+          <li>
+            <p className="text-base font-medium">
+              {education.title_program}, {education.university}
+            </p>
+          </li>
+          <li>
+            <p className="text-sm font-light text-[#686868]">
+              {education.education_program}
+            </p>
+          </li>
+        </ul>
+      </div>
+    ),
+
+    dot: (
+      <CustomDot
+        onClick={() => handleDotClick(index)}
+        selected={selectedDot === index}
+      />
+    ),
+  }));
+
   return (
     <section id="profile-education-section" className="mt-10">
       <ConfigProvider
         theme={{
           components: {
             Timeline: {
-              dotBorderWidth: 6,
-              itemPaddingBottom: "50px",
+              itemPaddingBottom: "20px",
               tailColor: "rgba(217, 217, 217, 0.7)",
             },
           },
@@ -20,9 +71,8 @@ export default function Education() {
         }}
       >
         <Timeline
-          items={education}
+          items={educationItems}
           mode="left"
-          className=""
           style={{ display: "inline-block" }}
         />
       </ConfigProvider>
---
+++ src/views/app-views/profile/constant/data-education/index.js
@@ -0,0 +1,34 @@
+export const DataEducations = [
+  {
+    id: 1,
+    starting_date: "2013",
+    end_date: "2015",
+    title_program: "Program Magister Kedokteran",
+    education_program: "Program Kedokteran",
+    university: "Universitas Gadjah Mada",
+  },
+  {
+    id: 2,
+    starting_date: "2009",
+    end_date: "2013",
+    title_program: "Program Residen Dokter Spesialis",
+    education_program: "Residency Program Kedokteran Internal",
+    university: "Rumah Sakit Sejahtera",
+  },
+  {
+    id: 3,
+    starting_date: "2006",
+    end_date: "2007",
+    title_program: "Program Internship Dokter Umum",
+    education_program: "",
+    university: "RSUP Klaten",
+  },
+  {
+    id: 4,
+    starting_date: "2000",
+    end_date: "2006",
+    title_program: "Sarjana Kedokteran",
+    education_program: "",
+    university: "Universitas Gadjah Mada, Fakultas Kedokteran",
+  },
+];
