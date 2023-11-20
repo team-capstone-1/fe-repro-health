fb00e717472c515fceab159f716f0dde8ec42910
Aurelia Amanda Robert <aurelia.amanda@ui.ac.id>
2023-11-20 19:36:15 +0800

    fix: Make doctor profile section responsively and fix constant

--- src/views/app-views/profile/components/WorkExperience.jsx
+++ src/views/app-views/profile/components/WorkExperience.jsx
@@ -12,14 +12,10 @@ export default function WorkExperience() {
         border: "2px solid #14C6A4",
         borderRadius: "50%",
         backgroundColor: selected ? "#14C6A4" : "#FFFFFF",
-        // marginBlock: 0,
-        // margin: 0,
-        // padding: 0,
       }}
     />
   );
 
-  // const [selectedDot, setSelectedDot] = useState(null);
   const [selectedDot, setSelectedDot] = useState(0);
 
   const handleDotClick = (index) => {
@@ -27,11 +23,6 @@ export default function WorkExperience() {
   };
 
   const timelineItems = data.map((items, index) => ({
-    // label: (
-    //   <p className="mr-3 text-left text-base font-bold text-grey-900">
-    //     {items.year}
-    //   </p>
-    // ),
     label: <p className="me-[90px] font-semibold">{items.year}</p>,
     dot: (
       <CustomDot
@@ -44,22 +35,13 @@ export default function WorkExperience() {
         <div className="w-[500px]">
           <p className="w-[50rem] font-medium">{items.job}</p>
         </div>
-        {items.description.map((desc, descIndex) => (
+        {items.position.map((desc, descIndex) => (
           <ul key={descIndex} className="w-[500px] ">
             <li className="ml-3 w-[50rem] text-sm font-light text-[#686868]">
               &bull; {desc}
             </li>
           </ul>
         ))}
-        {/* <p className="ml-3 font-semibold text-grey-900">{items.job}</p>
-        {items.description.map((desc, descIndex) => (
-          <p
-            key={descIndex}
-            className="ml-5 text-base font-normal text-grey-300"
-          >
-            &bull; {desc}
-          </p>
-        ))} */}
       </div>
     ),
   }));
