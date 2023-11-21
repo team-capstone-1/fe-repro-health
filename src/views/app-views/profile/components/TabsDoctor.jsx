c781e41c1bfbdc4939f239d3f303d44ab7c31a08
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-11-21 15:21:35 +0700

    refactor: (ProfileDoctor) add TabsDoctorProfile component

--- src/views/app-views/profile/components/TabsDoctor.jsx
+++ src/views/app-views/profile/components/TabsDoctor.jsx
@@ -1,48 +1,22 @@
-import { Card, ConfigProvider, Tabs } from "antd";
+import { Card } from "antd";
+import { TabsDoctorProfile } from "@/components/shared-components/Tabs";
 
 import WorkExperience from "./WorkExperience";
 import Education from "./Education";
 import Certificate from "./Certificate";
 
 export default function TabsDoctor() {
-  const onChange = (key) => {
-    console.log(key);
-  };
-
-  const tabs = [
-    {
-      key: "1",
-      label: <p className="w-[420px] text-center">Pengalaman kerja</p>,
-      children: <WorkExperience />,
-    },
-    {
-      key: "2",
-      label: <p className="w-[420px] text-center">Pendidikan</p>,
-      children: <Education />,
-    },
-    {
-      key: "3",
-      label: <p className="w-[420px] text-center">Sertifikasi</p>,
-      children: <Certificate />,
-    },
-  ];
-
   return (
     <section id="tabs-profile">
-      <ConfigProvider
-        theme={{
-          components: {
-            Tabs: {},
-          },
-          token: {
-            colorPrimary: "#14c6a4",
-          },
-        }}
-      >
-        <Card>
-          <Tabs defaultActiveKey="3" items={tabs} onChange={onChange} />
-        </Card>
-      </ConfigProvider>
+      <Card>
+        <TabsDoctorProfile
+          title={["Pengalaman Kerja", "Pendidikan", "Sertifikat"]}
+        >
+          <WorkExperience />
+          <Education />
+          <Certificate />
+        </TabsDoctorProfile>
+      </Card>
     </section>
   );
 }
