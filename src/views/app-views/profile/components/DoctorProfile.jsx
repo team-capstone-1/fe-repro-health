0eb34bf42ef280a275a02cf9912bf88c541977a4
amorazl <alfhiyana11@gmail.com>
2023-11-22 13:45:02 +0700

    refactor: fix profile doctor responsive

--- src/views/app-views/profile/components/DoctorProfile.jsx
+++ src/views/app-views/profile/components/DoctorProfile.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import profileDoctor from "@/assets/profile-doctor.svg";
 import { Card } from "antd";
 
@@ -7,26 +6,32 @@ export default function DoctorProfile() {
     <>
       <Card>
         <h3>Profil</h3>
-        <div className="flex flex-col md:flex-row items-center md:space-x-7">
-          <div id="doctor-image" >
-            <img
-              className="h-48 w-48 md:w-40"
-              id="doctor-image"
-              src={profileDoctor}
-              alt="profile-doctor"
-            />
+        <div className="items-center space-y-4 md:grid md:grid-cols-2 lg:grid-cols-12">
+          <div className="flex justify-center md:col-span-1 lg:col-span-3 xl:col-span-2">
+            <div id="doctor-image">
+              <img
+                className="my-5 h-48 w-48 md:my-0 md:w-36"
+                src={profileDoctor}
+                alt="profile-doctor"
+              />
+            </div>
           </div>
-          <div className="md:flex md:flex-row md:space-x-7 md:space-y-0 md:text-left flex flex-col my-5 mx-10 text-center space-y-7">
-            <div className="flex flex-col" id="doctor-information">
+          <div className="flex justify-start md:col-span-1 lg:col-span-4 lg:justify-center xl:col-span-3">
+            <div id="doctor-information" className="flex flex-col">
               <p className="text-sm font-semibold text-grey-900 md:text-base">
                 Dr. Andi Cahaya, Sp.OG
               </p>
-              <p className="-mt-1 text-sm font-medium text-grey-300 md:text-base">
+              <p className="text-sm font-medium text-grey-300 md:text-base">
                 Spesialis Kandungan
               </p>
-              <p className="mt-2 capitalize">10 tahun pengalaman</p>
+              <p className="mt-2">10 tahun pengalaman</p>
             </div>
-            <div className="md:flex md:w-[40%] md:space-x-3" id="doctor-address">
+          </div>
+          <div className="me-5 md:col-span-1 lg:col-span-7 xl:col-span-4">
+            <div
+              id="doctor-address"
+              className="xl:flex xl:w-[100%] xl:space-x-3"
+            >
               <p className="text-sm font-semibold text-grey-900 md:text-base">
                 Alamat
               </p>
@@ -35,7 +40,9 @@ export default function DoctorProfile() {
                 Kec. Depok, Kabupaten Sleman, DIY
               </p>
             </div>
-            <div className="flex space-x-3 justify-center text-left" id="doctor-contacts">
+          </div>
+          <div className="flex md:col-span-1 lg:col-span-5 xl:col-span-3">
+            <div id="doctor-contacts" className="flex space-x-3 text-left">
               <div className="flex-col">
                 <p className="text-sm font-semibold text-grey-900 md:text-base">
                   Email
