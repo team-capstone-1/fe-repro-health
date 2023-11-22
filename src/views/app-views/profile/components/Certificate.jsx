e726033f30c59998f9c4f467ee5785d9bec649af
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-11-22 16:01:39 +0700

    refactor display certificate

--- src/views/app-views/profile/components/Certificate.jsx
+++ src/views/app-views/profile/components/Certificate.jsx
@@ -1,13 +1,16 @@
 import { Col, ConfigProvider, Form, Image, Input, Row, Table } from "antd";
 import { IoMdSearch } from "react-icons/io";
 
-import IconShowCertif from "@/assets/certif-show.svg";
+// import IconShowCertif from "@/assets/certif-show.svg";
 import ImageCertif from "@/assets/certificate-dental.png";
+import { useState } from "react";
 
 export default function Certificate() {
+  const [visible, setVisible] = useState(false);
+
   const DataSource = [
     {
-      id: "f03f5d62-5cf3-4f03-9c3f-8c34620e94c6",
+      id: 1,
       doctor_profile_id: "74300897765",
       time_period: "22 Juni 2022 - 22 Juni 2027",
       // time_period: {
@@ -17,88 +20,59 @@ export default function Certificate() {
       description: "Praktik Medis",
       certificate_type: "Sertifikasi Lisensi",
       file_size: "5 MB",
-      details: (
-        <Image
-          width={20}
-          src={IconShowCertif}
-          preview={{
-            src: ImageCertif,
-          }}
-        />
-      ),
+      details: ImageCertif,
     },
 
-    {
-      id: "f03f5d62-5cf3-4f03-9c3f-8c34620e94c6",
-      doctor_profile_id: "74300897765",
-      time_period: "22 Juni 2022 - 22 Juni 2027",
-      description: "Praktik Medis",
-      certificate_type: "Sertifikasi Lisensi",
-      file_size: "5 MB",
-      details: (
-        <Image
-          width={20}
-          src={IconShowCertif}
-          preview={{
-            src: "https://yt3.googleusercontent.com/Yltbh2eH6nCCEwh4au4IeBK-FNkYSETVxLngkPbcj1igZ__PJa4XVEt-0Jx-sGAx717p1kERzqA=s900-c-k-c0x00ffffff-no-rj",
-          }}
-        />
-      ),
-    },
+    // {
+    //   id: 2,
+    //   doctor_profile_id: "74300897765",
+    //   time_period: "22 Juni 2022 - 22 Juni 2027",
+    //   description: "Praktik Medis",
+    //   certificate_type: "Sertifikasi Lisensi",
+    //   file_size: "5 MB",
+    //   details:
+    //     "https://yt3.googleusercontent.com/Yltbh2eH6nCCEwh4au4IeBK-FNkYSETVxLngkPbcj1igZ__PJa4XVEt-0Jx-sGAx717p1kERzqA=s900-c-k-c0x00ffffff-no-rj",
+    // },
 
-    {
-      id: "f03f5d62-5cf3-4f03-9c3f-8c34620e94c6",
-      doctor_profile_id: "74300897765",
-      time_period: "22 Juni 2022 - 22 Juni 2027",
-      description: "Praktik Medis",
-      certificate_type: "Sertifikasi Lisensi",
-      file_size: "5 MB",
-      details: (
-        <Image
-          width={20}
-          src={IconShowCertif}
-          preview={{
-            src: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-          }}
-        />
-      ),
-    },
+    // {
+    //   id: 3,
+    //   doctor_profile_id: "74300897765",
+    //   time_period: "22 Juni 2022 - 22 Juni 2027",
+    //   description: "Praktik Medis",
+    //   certificate_type: "Sertifikasi Lisensi",
+    //   file_size: "5 MB",
+    //   details:
+    //     "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    // },
 
-    {
-      id: "f03f5d62-5cf3-4f03-9c3f-8c34620e94c6",
-      doctor_profile_id: "74300897765",
-      time_period: "22 Juni 2022 - 22 Juni 2027",
-      description: "Praktik Medis",
-      certificate_type: "Sertifikasi Lisensi",
-      file_size: "5 MB",
-      details: (
-        <Image
-          width={20}
-          src={IconShowCertif}
-          preview={{
-            src: "https://assets-a1.kompasiana.com/items/album/2022/05/30/eren-yeager-1-6294723353e2c32b47142933.jpg",
-          }}
-        />
-      ),
-    },
+    // {
+    //   id: 4,
+    //   doctor_profile_id: "74300897765",
+    //   time_period: "22 Juni 2022 - 22 Juni 2027",
+    //   description: "Praktik Medis",
+    //   certificate_type: "Sertifikasi Lisensi",
+    //   file_size: "5 MB",
+    //   details:
+    //     "https://assets-a1.kompasiana.com/items/album/2022/05/30/eren-yeager-1-6294723353e2c32b47142933.jpg",
+    // },
 
-    {
-      id: "f03f5d62-5cf3-4f03-9c3f-8c34620e94c6",
-      doctor_profile_id: "74300897765",
-      time_period: "22 Juni 2022 - 22 Juni 2027",
-      description: "Praktik Medis",
-      certificate_type: "Sertifikasi Lisensi",
-      file_size: "5 MB",
-      details: (
-        <Image
-          width={20}
-          src={IconShowCertif}
-          preview={{
-            src: "https://i.ytimg.com/vi/f93sHJkdsiw/maxresdefault.jpg",
-          }}
-        />
-      ),
-    },
+    // {
+    //   id: "f03f5d62-5cf3-4f03-9c3f-8c34620e94c6",
+    //   doctor_profile_id: "74300897765",
+    //   time_period: "22 Juni 2022 - 22 Juni 2027",
+    //   description: "Praktik Medis",
+    //   certificate_type: "Sertifikasi Lisensi",
+    //   file_size: "5 MB",
+    //   details: (
+    //     <Image
+    //       width={20}
+    //       src={IconShowCertif}
+    //       preview={{
+    //         src: "https://i.ytimg.com/vi/f93sHJkdsiw/maxresdefault.jpg",
+    //       }}
+    //     />
+    //   ),
+    // },
   ];
 
   const columns = [
@@ -132,13 +106,13 @@ export default function Certificate() {
       key: "file_size",
       width: 100,
     },
-    {
-      title: "Detail",
-      dataIndex: "details",
-      key: "details",
-      width: 100,
-      align: "center",
-    },
+    // {
+    //   title: "Detail",
+    //   dataIndex: "details",
+    //   key: "details",
+    //   width: 100,
+    //   align: "center",
+    // },
   ];
 
   return (
@@ -185,6 +159,25 @@ export default function Certificate() {
                 style={{ maxWidth: "100%" }}
                 className="shadow-sm"
                 bordered
+                onRow={() => ({
+                  onClick: () => {
+                    setVisible(true);
+                  },
+                })}
+              />
+              <Image
+                width={200}
+                style={{
+                  display: "none",
+                }}
+                src={DataSource.map((val) => val.details)}
+                preview={{
+                  visible,
+                  // src: ,
+                  onVisibleChange: (value) => {
+                    setVisible(value);
+                  },
+                }}
               />
             </ConfigProvider>
           </Col>
