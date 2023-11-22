f383cf6722b87f5f435b9ea1dc60000ed9aeb650
Muhamad Rhamdhan Mardiansyah <ram.ardiansyah18@gmail.com>
2023-11-22 19:29:56 +0700

    refactor : change file name from bahasa to english

--- src/views/app-views/notification/Index.jsx
+++ src/views/app-views/notification/Index.jsx
@@ -1,31 +1,32 @@
 // YourOtherFile.js
-import React, { useState } from "react";
-import { Tabs } from '../../../components/shared-components/Tabs'
-import BelumDibaca from "./components/BelumDibaca";
-import Semua from "./components/Semua";
+import { useState } from "react";
+import { Tabs } from "../../../components/shared-components/Tabs";
+
+import AllNotification from "./components/AllNotification";
+import UnRead from "./components/UnRead";
 
 function Notification() {
-    const [jumlahBelum, setJumlahBelum] = useState('')
-    const [jumlahSudah, setJumlahSudah] = useState('')
+  const [jumlahBelum, setJumlahBelum] = useState("");
+  const [jumlahSudah, setJumlahSudah] = useState("");
 
-    const jumlahBelumSekarang = (berapa) => {
-        setJumlahBelum(berapa)
-    }
-    const jumlahSemuaSekarang = (berapa) => {
-        setJumlahSudah(berapa)
-    }
-    return (
+  const jumlahBelumSekarang = (berapa) => {
+    setJumlahBelum(berapa);
+  };
+  const jumlahSemuaSekarang = (berapa) => {
+    setJumlahSudah(berapa);
+  };
+  return (
+    <div>
+      <Tabs title={[`Semua(${jumlahSudah})`, `Belum dibaca(${jumlahBelum})`]}>
         <div>
-            <Tabs title={[`Semua(${jumlahSudah})`, `Belum dibaca(${jumlahBelum})`]}>
-                <div>
-                    <Semua dataSudah={jumlahSemuaSekarang} />
-                </div>
-                <div>
-                    <BelumDibaca dataBelum={jumlahBelumSekarang} />
-                </div>
-            </Tabs>
+          <AllNotification dataSudah={jumlahSemuaSekarang} />
         </div>
-    );
+        <div>
+          <UnRead dataBelum={jumlahBelumSekarang} />
+        </div>
+      </Tabs>
+    </div>
+  );
 }
 
 export default Notification;
