dfba7dff07e844f1c52ea9875ac5a84d3759e6be
Naufal Nasrullah <naufaln176@gmail.com>
2023-11-24 07:13:38 +0700

    refactor: auto scroll to bottom on chat and adjust styling

--- src/views/app-views/chatbot/index.jsx
+++ src/views/app-views/chatbot/index.jsx
@@ -1,12 +1,15 @@
 import { Button } from "antd";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import ChatbotIcon from "@/assets/chatbot-icon.svg";
 import ChatbotMascot from "@/assets/chatbot-mascot.svg";
 import { LuSendHorizonal } from "react-icons/lu";
 import { useForm } from "react-hook-form";
+import profileDoctor from "@/assets/profile-doctor.svg";
 
 export default function Chatbot() {
   const { register, handleSubmit } = useForm();
+  const [lists, setLists] = useState(initialLists);
+  const ref = useRef(null);
   const [selectedList, setSelectedList] = useState([
     {
       id: null,
@@ -15,7 +18,6 @@ export default function Chatbot() {
       pesan: [],
     },
   ]);
-  const [lists, setLists] = useState(initialLists);
 
   const date = new Date();
   const formattedDate = date.toLocaleDateString("id-ID", {
@@ -42,10 +44,19 @@ export default function Chatbot() {
     console.log(data);
   };
 
+  useEffect(() => {
+    if (lists.length) {
+      ref.current?.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+      });
+    }
+  }, [lists.length, selectedList]);
+
   return (
     <>
-      <div className="sm:max-md:h-[calc(100vh-82.6px) flex min-h-[500px] flex-col-reverse gap-8 px-2 py-5 sm:h-[calc(100vh-75.91px)] sm:flex-row 2xl:px-4">
-        <div className="h-full max-w-[400px] overflow-y-scroll rounded-md p-4 ring-1 ring-slate-200 sm:w-5/12 [&::-webkit-scrollbar-thumb]:bg-slate-400 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar]:w-[4px]">
+      <div className="sm:max-md:h-[calc(100vh-82.6px) flex min-h-[500px] flex-col-reverse gap-8 px-2 py-5 sm:h-[calc(100vh-81.8px)] sm:flex-row 2xl:px-4">
+        <div className="h-full overflow-y-scroll rounded-md p-4 ring-1 ring-slate-200 sm:w-5/12 sm:max-w-[400px] [&::-webkit-scrollbar-thumb]:bg-slate-400 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar]:w-[4px]">
           <h1 className="text-2xl">Asisten Dokter</h1>
           <div className="py-5">
             <Button
@@ -87,8 +98,8 @@ export default function Chatbot() {
             <img src={ChatbotIcon} alt="" />
             <h5 className="font-semibold">Asisten Dokter</h5>
           </div>
-          <div className="flex min-h-[380px] items-center justify-center sm:h-[calc(100%-165px)]">
-            <div className="flex h-full w-full flex-col overflow-y-scroll px-8 [&::-webkit-scrollbar-thumb]:bg-slate-400 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar]:w-[4px]">
+          <div className="flex h-[calc(100%-165px)] items-center justify-center">
+            <div className="flex h-full max-h-[80vh] w-full flex-col overflow-y-scroll px-2 min-[500px]:px-6 [&::-webkit-scrollbar-thumb]:bg-slate-400 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar]:w-[4px]">
               {selectedList[0]?.pesan.length === 0 ? (
                 <div className="my-auto">
                   <img className="mx-auto" src={ChatbotMascot} alt="" />
@@ -108,39 +119,65 @@ export default function Chatbot() {
                   >
                     <div
                       className={`flex flex-col gap-1.5 ${
-                        pesan.pengirim === "user"
-                          ? "items-end"
-                          : "items-start text-white"
+                        pesan.pengirim === "user" ? "items-end" : "items-start"
                       }`}
                     >
                       <div
-                        className={`flex items-center justify-center rounded-md px-3 py-2.5 ${
-                          pesan.pengirim === "user"
-                            ? "bg-green-500 text-white"
-                            : "bg-slate-300"
+                        className={`flex gap-3 ${
+                          pesan.pengirim === "user" && "flex-row-reverse"
                         }`}
                       >
-                        <p className="overflow-hidden text-ellipsis whitespace-nowrap pr-4 text-sm font-medium">
-                          {pesan.pesan}
-                        </p>
+                        <img
+                          src={
+                            pesan.pengirim === "user"
+                              ? profileDoctor
+                              : ChatbotIcon
+                          }
+                          className="hidden h-8 w-8 min-[500px]:block"
+                          alt="chatbot icon"
+                        />
+                        <div
+                          className={`${
+                            pesan.pengirim === "user" && "text-right"
+                          }`}
+                        >
+                          <p>
+                            {pesan.pengirim === "user"
+                              ? "Anda"
+                              : "Asisten Dokter"}
+                          </p>
+                          <div
+                            className={`flex w-fit items-center justify-center rounded-md px-1.5 py-2.5 min-[500px]:px-3 ${
+                              pesan.pengirim === "user"
+                                ? "bg-green-500 text-white"
+                                : "bg-slate-300"
+                            }`}
+                          >
+                            <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-medium">
+                              {pesan.pesan}
+                            </p>
+                          </div>
+                        </div>
                       </div>
-                      <p className="text-xs text-slate-500">{pesan.waktu}</p>
+                      {/* <p className="text-xs text-slate-500">{pesan.waktu}</p> */}
                     </div>
                   </div>
                 ))
               )}
+              <div ref={ref}></div>
             </div>
           </div>
           <div className="px-6 pb-6 pt-4 sm:pb-0">
             <form onSubmit={handleSubmit(onSubmit)}>
               <div className="relative flex focus:bg-black">
-                <input
+                <textarea
                   id="input-chatbot"
                   {...register("input-chatbot")}
                   type="text"
-                  className="block w-full rounded-md border-2 border-gray-300 px-6 py-3 focus:outline-green-500"
+                  className="block h-12 w-full resize-none overflow-y-scroll rounded-md border-2 border-gray-300 px-6 pr-10 pt-[0.8em] focus:outline-green-500 [&::-webkit-scrollbar-thumb]:bg-slate-400 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar]:w-[4px]"
                   placeholder="Kirim pesan di sini..."
                   required
+                  maxLength={1024}
                 />
                 <button
                   type="submit"
