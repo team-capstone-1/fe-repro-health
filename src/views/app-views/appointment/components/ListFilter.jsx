3209ba5557e53de04e2644a6578ef3e70911c326
amorazl <alfhiyana11@gmail.com>
2023-11-25 11:34:51 +0700

    refactor: change border form color

--- src/views/app-views/appointment/components/ListFilter.jsx
+++ src/views/app-views/appointment/components/ListFilter.jsx
@@ -1,4 +1,5 @@
-import { Col, Form, Input, Row, Select } from "antd";
+import { Col, Form, Input, Row, Select, ConfigProvider } from "antd";
+import { IoMdSearch } from "react-icons/io";
 
 export default function ListFilter() {
   const Filter = [
@@ -19,37 +20,56 @@ export default function ListFilter() {
   return (
     <>
       <Form layout="vertical">
-        <Row gutter={[16, 8]}>
-          <Col span={24} md={8}>
-            <p
-              id="appointment-table-title"
-              className="mb-4 text-2xl font-semibold"
+        <ConfigProvider
+          theme={{
+            components: {
+              Input: {
+                colorPrimary: "#17c6a3",
+              },
+              Select: {
+                colorPrimary: "#17c6a3",
+              },
+            },
+          }}
+        >
+          <Row gutter={[16, 8]}>
+            <Col span={24} md={8}>
+              <p
+                id="appointment-table-title"
+                className="mb-4 text-2xl font-semibold"
+              >
+                Daftar Janji Temu
+              </p>
+            </Col>
+            <Col
+              span={24}
+              md={{ span: 8, offset: 2 }}
+              lg={{ span: 7, offset: 4 }}
+              xl={{ span: 6, offset: 6 }}
+              className="text-end"
             >
-              Daftar Janji Temu
-            </p>
-          </Col>
-          <Col
-            span={24}
-            md={{ span: 8, offset: 2 }}
-            lg={{ span: 7, offset: 4 }}
-            xl={{ span: 6, offset: 6 }}
-            className="text-end"
-          >
-            <Form.Item name="search">
-              <Input.Search placeholder="Cari Janji Temu..." />
-            </Form.Item>
-          </Col>
-          <Col span={24} md={{ span: 6 }} lg={{ span: 5 }} xl={{ span: 4 }}>
-            <Form.Item name="filter">
-              <Select
-                placeholder="Filter"
-                options={Filter}
-                showSearch
-                allowClear
-              />
-            </Form.Item>
-          </Col>
-        </Row>
+              <Form.Item id="search-appointment" name="search">
+                <Input
+                  placeholder="Cari Janji Temu..."
+                  allowClear
+                  prefix={<IoMdSearch />}
+                  className="h-[2.1rem] hover:border-green-500"
+                />
+              </Form.Item>
+            </Col>
+            <Col span={24} md={{ span: 6 }} lg={{ span: 5 }} xl={{ span: 4 }}>
+              <Form.Item id="filter-appointment" name="filter">
+                <Select
+                  placeholder="Filter"
+                  options={Filter}
+                  showSearch
+                  allowClear
+                  className="h-[2.1rem] hover:border-green-500"
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+        </ConfigProvider>
       </Form>
     </>
   );
