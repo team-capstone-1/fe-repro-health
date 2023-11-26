ba15de37899870879dd495fa1c04a0eaed420bcb
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-11-26 20:59:50 +0700

    add responsive notfound

--- src/views/error-views/Notfound.jsx
+++ src/views/error-views/Notfound.jsx
@@ -1,21 +1,28 @@
 import { Button } from "antd";
 import { Link } from "react-router-dom";
 
+import imageNotfound from "@/assets/notfound.svg";
+
 export default function Notfound() {
   return (
-    <section id="error-notfound">
-      <div className="bg-error-notfound flex h-screen w-screen items-center justify-center bg-center bg-no-repeat text-center">
-        <div className="mt-[35%]">
-          <h4 className="text-2xl">Halaman Tidak Ditemukan!</h4>
-          <p className="text-sm text-[#686868]">
-            Link halaman yang anda tuju tidak ditemukan di server ini.
-          </p>
-          <Link to="/">
-            <Button className="mt-[1rem] h-[50px] w-[100%] bg-green-500 text-base text-white hover:border-transparent">
-              Kembali ke halaman utama
-            </Button>
-          </Link>
-        </div>
+    <section
+      id="error-notfound"
+      className="m-auto mt-[50%] flex h-[100vw] w-[90vw] flex-col items-center justify-center text-center sm:mt-0 sm:w-[80vw] md:mt-0 md:h-[80vw] lg:mt-0 lg:h-[60vw] lg:w-[100vw] xl:h-[45vw]"
+    >
+      <img
+        src={imageNotfound}
+        className="h-[40%] items-center justify-center"
+      />
+      <div className="mt-[3rem]">
+        <h4 className="text-base lg:text-2xl">Halaman Tidak Ditemukan!</h4>
+        <p className="my-[1rem] text-xs text-[#686868] lg:text-sm">
+          Link halaman yang anda tuju tidak ditemukan di server ini.
+        </p>
+        <Link to="/">
+          <Button className="mt-[1rem] h-[50px] w-[100%] bg-green-500 text-base text-white hover:border-transparent">
+            Kembali ke halaman utama
+          </Button>
+        </Link>
       </div>
     </section>
   );
