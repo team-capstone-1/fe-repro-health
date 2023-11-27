afc4af6a9525116f3cb130b2c65b2fa598eebd81
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-11-27 12:09:50 +0700

    add id button error notfound

--- src/views/error-views/Notfound.jsx
+++ src/views/error-views/Notfound.jsx
@@ -5,10 +5,7 @@ import imageNotfound from "@/assets/notfound.svg";
 
 export default function Notfound() {
   return (
-    <section
-      id="error-notfound"
-      className="m-auto mt-[50%] flex h-[100vw] w-[90vw] flex-col items-center justify-center text-center sm:mt-0 sm:w-[80vw] md:mt-0 md:h-[80vw] lg:mt-0 lg:h-[60vw] lg:w-[100vw] xl:h-[45vw]"
-    >
+    <section className="m-auto mt-[50%] flex h-[100vw] w-[90vw] flex-col items-center justify-center text-center sm:mt-0 sm:w-[80vw] md:mt-0 md:h-[80vw] lg:mt-0 lg:h-[60vw] lg:w-[100vw] xl:h-[45vw]">
       <img
         src={imageNotfound}
         className="h-[40%] items-center justify-center"
@@ -18,7 +15,7 @@ export default function Notfound() {
         <p className="my-[1rem] text-xs text-[#686868] lg:text-sm">
           Link halaman yang anda tuju tidak ditemukan di server ini.
         </p>
-        <Link to="/">
+        <Link to="/" id="btn-error-notfound">
           <Button className="mt-[1rem] h-[50px] w-[100%] bg-green-500 text-base text-white hover:border-transparent">
             Kembali ke halaman utama
           </Button>
