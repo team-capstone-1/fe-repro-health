358e8c3691c3161ba8f4e6b84c4c3f664994add5
aaaroz <ram.ardiansyah18@gmail.com>
2023-11-28 18:25:01 +0700

    refactor (tooltip) : set a span as wrapper for tooltip

--- src/views/app-views/notification/components/ListNotifications.jsx
+++ src/views/app-views/notification/components/ListNotifications.jsx
@@ -28,7 +28,9 @@ export default function ListNotifications() {
       <Flex>
         <h3 className="mb-3 font-bold">Notifikasi ({totalUnRead})</h3>
         <Tooltip placement="right" title={text} color="white" arrow={true}>
-          <FiInfo className="text-lg" />
+          <span>
+            <FiInfo className="text-lg" />
+          </span>
         </Tooltip>
       </Flex>
       {DataNotifications.map(
