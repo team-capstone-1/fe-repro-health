05ba10200ddde829f7cf928cc39f459d3250124d
Naufal Nasrullah <naufaln176@gmail.com>
2023-11-30 05:21:40 +0700

    refactor: add axios interceptor

--- src/configs/AxiosInstance.js
+++ src/configs/AxiosInstance.js
@@ -1,6 +1,18 @@
 import axios from "axios";
 import { CONST } from "@/utils/Constant";
+import { authService } from "@/configs/Auth";
 
 export const axiosInstance = axios.create({
   baseURL: CONST.BASE_URL_API,
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = authService.getToken();
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
